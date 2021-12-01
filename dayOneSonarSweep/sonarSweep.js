87580385559116556354b95f036df3dcb5214a1a
matthew-hre <whycardboard@gmail.com>
2021-12-01 08:16:02 -0700

    day 1 revision

--- dayOneSonarSweep/sonarSweep.js
+++ dayOneSonarSweep/sonarSweep.js
@@ -1,22 +1,21 @@
 function readInputs() {
     var fs = require('fs');
     var contents = fs.readFileSync('C:\\Users\\matth\\Documents\\PROJECTS\\js projects\\adventOfCode2021\\dayOneSonarSweep\\inputs.txt', 'utf8');
-    var inputs = contents.split('\n');
+    var inputs = contents.split('\n').map(x => parseInt(x));
     return inputs;
 }
 
 function sonarSweep() {
     var inputArray = readInputs();
-    var output = 0;
-    for (var i = 0; i < inputArray.length; i++) {
-        if(i === 0) {
-            continue;
+    var output = inputArray.reduce((prev, curr, currIdx, arr) => {
+        if (currIdx === 0) {
+            return 0;
         }
-
-        if(parseInt(inputArray[i]) > parseInt(inputArray[i-1])) {
-            output++;
+        if (curr > arr[currIdx - 1]) {
+            return prev + 1;
         }
-    }
+        return prev;
+    }, 0);
 
     console.log("sonar sweep increases: " + output);
 }
